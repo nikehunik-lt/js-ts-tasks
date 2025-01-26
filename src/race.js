04b5ec35d53ba9583b9287740607777b9ef44f55
nikehunik-lt <stepanenko.nikita@student.ehu.lt>
2025-01-26 22:58:09 +0200

    feat hometasks-01-html-css

---
+++ src/race.js
@@ -0,0 +1,21 @@
+/**
+ * Write a function that will work similar to standard Promise.race
+ * @param {Array<Promise>} promisesArray
+ * @returns Promise
+ */
+module.exports.race = function race (promisesArray) {
+  return new Promise((res, rej) => {
+    let i = 0;
+    while (i < promisesArray.length) {
+      promisesArray[i].then(
+        value => {
+          res(value);
+        },
+        reason => {
+          rej(reason);
+        }
+      );
+      i++;
+    }
+  });
+};
